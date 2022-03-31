e45213fcc20b0c6d3c73c7bab2dbe94149389181
choeyoha <yoha6865@naver.com>
2022-03-31 11:03:13 +0900

    220331 login, signup

--- app/src/main/java/com/example/mny/LoginActivity.java
+++ app/src/main/java/com/example/mny/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.mny;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button signup = (Button)findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(SignupActivity.class);
+            }
+        });
     }
+
+    private void startActivity(Class c) {
+        Intent intent = new Intent(this, c);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
 }
