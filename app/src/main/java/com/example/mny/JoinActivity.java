d7cc1bff6ea100c088ab1a8171c4015a64bac358
choeyoha <yoha6865@naver.com>
2022-05-03 15:14:10 +0900

    220502 login_xml

--- app/src/main/java/com/example/mny/JoinActivity.java
+++ app/src/main/java/com/example/mny/JoinActivity.java
@@ -1,4 +1,30 @@
 package com.example.mny;
 
-public class JoinActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class JoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+
+
+    }
+
+
+
+
+
+
+    private void startActivity(Class c) {
+        Intent intent = new Intent(this, c);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
 }
